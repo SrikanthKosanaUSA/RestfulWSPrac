f657348622634f39ff2924584dc30790912d89ec
Srikanth <Srikanth@10.0.0.149>
2018-05-03 23:26:52 -0500

    After adding list of students in the resource file......

--- RestPrac/src/main/java/com/srikanth/restfulprac/RestPrac/StudentResource.java
+++ RestPrac/src/main/java/com/srikanth/restfulprac/RestPrac/StudentResource.java
@@ -1,5 +1,8 @@
 package com.srikanth.restfulprac.RestPrac;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,14 +13,26 @@ public class StudentResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public Student getStudent() {
+	public List<Student> getAllStudents() {
 		Student s1 = new Student();
-	
-		s1.setStudentID(1);
+	    s1.setStudentID(1);
 		s1.setFirstName("One");
 		s1.setLastName("EKH");
 		
-		return s1;
+		Student s2 = new Student();
+	    s2.setStudentID(2);
+		s2.setFirstName("Two");
+		s2.setLastName("Dho");
+		
+		Student s3 = new Student();
+	    s3.setStudentID(3);
+		s3.setFirstName("Three");
+		s3.setLastName("Theen");
+		
+		List<Student> studentslist = Arrays.asList(s1, s2, s3);
+		
+		
+		return studentslist;
 		
 	}
 }
